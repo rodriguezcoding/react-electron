3dc7eaa90f4bccd5d383081d6ace0d809bb11b40
rodriguezcoding <luis.rodriguezcastro31@gmail.com>
2019-08-02 07:50:42 -0400

    Update package

--- public/main.js
+++ public/main.js
@@ -18,8 +18,7 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true
+      preload: path.join(app.getAppPath(), "preload.js")
     }
   });
 
